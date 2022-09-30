150838b79a174b40159d6f202709a4f5a22a3af8
isaac <isaac.gutierrez@ksms.mx>
2022-09-30 12:18:54 -0500

    Original

---
+++ Width adding.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+df=pd.read_csv('first_augmentations.csv')
+#%%
+heights=[]
+widths=[]
+for x in range(len(df)):
+    path=df['path'][x]
+    img=Image.open(path)
+    width, height = img.size
+    img=np.array(img)
+    heights.append(height)
+    widths.append(width)
+    # plt.imshow(img)
+    # plt.show()
+#%%
+df['width']=widths
+df['height']=heights
+#%%
+df.to_csv(r'C:\Users\isaac\PycharmProjects\tensorflow_filter\first_augmentations.csv',index=False)
